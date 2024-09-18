87eb04b29742337ef1ee2c7f8d27ff97d6676ade
Julian So <ch.julian.so@gmail.com>
2024-09-18 07:35:10 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// https://reactrouter.com/en/main/start/tutorial
+
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Shopping Cart</h1>
+    </>
+  );
+}
+
+export default App;
