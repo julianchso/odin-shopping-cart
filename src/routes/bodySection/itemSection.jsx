beb032d2add5f5e8c93be17c75129d36aa6425f6
Julian So <ch.julian.so@gmail.com>
2024-10-04 07:53:31 -0400

    changing fetch to use custom hook

--- src/routes/bodySection/itemSection.jsx
+++ src/routes/bodySection/itemSection.jsx
@@ -1,31 +1,61 @@
+import { useEffect, useState } from 'react';
+import {
+  fetchItemName,
+  fetchItemDetail,
+  useFetchItemName,
+} from '../services and helpers/fetchItem';
+
 import Card from './card';
-import { FetchItemName } from '../services and helpers/fetchItem';
 import PropTypes from 'prop-types';
 
 import '../../global.css';
 import './itemSection.css';
 
 export default function ItemSection() {
-  const getItems = async () => {
-    const items = await FetchItemName();
-    console.log(typeof items);
-    console.log(items);
-    return items;
-  };
+  // const [pokeItemDetail, setPokeItemDetail] = useState([]);
+  const [pokeItems] = useFetchItemName();
 
-  const getItemsfn = getItems();
-  console.log(getItemsfn);
-  // const items = await FetchItemName();
+  // version 1
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const limit = 10;
+  //     const url = `https://pokeapi.co/api/v2/item?limit=${limit}&offset=0`;
+  //     // console.log(pokeItem);
 
-  // console.log(items);
+  //     // fetch item name
+  //     await fetch(url, { mode: 'cors' })
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         console.log(data.results);
+  //         setPokeItem(data.results);
+  //       })
+  //       .catch((e) => console.error(e));
+  //   };
+
+  //   fetchData();
+  // }, []);
+
+  // version 2
+  // useEffect(() => {
+  //   (async () => {
+  //     const data = await fetchItemName();
+  //     console.log(data);
+  //     setPokeItem(data);
+  //     console.log(pokeItem);
+  //   })();
+  // }, []);
+
+  // console.log(pokeItem);
 
   return (
     <>
       <div className='itemSection'>
         <ul className='itemGridContainer'>
-          {/* {getItems.map((item) => {
-            return <Card item={item.name} key={item.id} className='itemCard' />;
-          })} */}
+          {pokeItems &&
+            Object.values(pokeItems).map((item) => {
+              console.log(item);
+              return <Card item={item} key={item.id} className='itemCard' />;
+            })}
         </ul>
       </div>
     </>
