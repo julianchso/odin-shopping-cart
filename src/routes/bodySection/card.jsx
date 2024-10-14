df2d963bb839cb4e39958f92c0b03b1133b6b3ad
Julian So <ch.julian.so@gmail.com>
2024-10-13 21:46:30 -0400

    add css to item card. Try to add increment/decrement to item qty

--- src/routes/bodySection/card.jsx
+++ src/routes/bodySection/card.jsx
@@ -1,26 +1,56 @@
 import PropTypes from 'prop-types';
 import { useFetchItemDetail } from '../services and helpers/fetchItem';
+import { useState } from 'react';
 
 import '../../global.css';
 import './card.css';
+import { useSearchParams } from 'react-router-dom';
 
 export default function Card({ item }) {
-  return (
-    <div className='card'>
-      <li>
-        <div className='cardName'>{item.name}</div>
-        <div className='cardImg'>
-          <img src={item.src} />
-        </div>
-        <div className='cardPrice'>{`$${item.price}`}</div>
-      </li>
+  // const Quantity = () => {
+  //   const [qty, setQty] = useState(0);
 
-      <div>
-        <button type='button'>-</button>
-        <input type='number' />
-        <button type='button'>+</button>
-        <button type='button'>Add to Cart</button>
+  //   decrementQty = () => {
+  //     if (qty <= 0) {
+  //       setQty(0);
+  //     } else {
+  //       setQty(qty - 1);
+  //     }
+  //   };
+
+  //   incrementQty = () => {
+  //     setQty(qty + 1);
+  //   };
+
+  //   function onChange(e) {
+  //     const val = e.target.value;
+
+  //     return val <= 0 ? setQty(0) : setQty(val);
+  //   }
+  // };
+
+  return (
+    <div className='cardCtn'>
+      <div className='cardName center'>{item.name}</div>
+      <div className='cardImg center'>
+        <img className='center' src={item.src} />
       </div>
+      <div className='cardPrice center'>{`$${item.price}`}</div>
+
+      <div className='inputQtyCtn'>
+        <button type='button'>-</button>
+        {/* <button type='button' onClick={this.decrementQty}>
+          -
+        </button> */}
+        <input className='inputQty' type='number' />
+        <button type='button'>+</button>
+        {/* <button type='button' onClick={this.incrementQty}>
+          +
+        </button> */}
+      </div>
+      <button type='button' className='addToCartBtn'>
+        Add to Cart
+      </button>
     </div>
   );
 }
